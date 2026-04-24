af7adc0a90416464cce55237b83918a3e2b31e45
det-core <darkempiretech@mail.com>
2026-04-24 20:28:01 +0100

    Some plugins fixed and some new features added

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ global.linkch = "https://whatsapp.com/channel/0029VbBwJYo6BIEp0Xlm1G0S"
 global.owner = ["2347030626048"] 
 global.nameowner = "ꪶ ¡ϻ Nᴜʟʟ ꫂ" 
 global.author = ["2347030626048"] 
-global.nameauthor = "ꪶ ¡ϻ Nᴜʟʟ ꫂ"" 
+global.nameauthor = "ꪶ ¡ϻ Nᴜʟʟ ꫂ" 
 global.namebot = "Nᴜʟʟ Cʀᴀsʜ V𝟷" 
 global.bot = {
   newsletterJid: 
